cc5ddd63b3ee02dbb271ccd8a1ec2b2014717a7a
DemNeg <basingoudembele@gmail.com>
2022-03-25 11:04:02 +0100

    Import modules and declare cmpts in app.module
    - The imoprted components are now part of the module
    - The imported modules can be used in the components of app.module
    
    - It mandatory to declare all the components of a particular module in
      the declarations : [] section
    - To use any kind of module in a particular module we need to add it in
      imports:[] section.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,49 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {BadgeModule} from 'primeng/badge';
+import { AvatarModule } from 'primeng/avatar';
+import {HttpClientModule} from '@angular/common/http';
+import { TableModule } from 'primeng/table';
+import { RippleModule } from 'primeng/ripple';
+import { PasswordModule } from 'primeng/password';
+import { CheckboxModule } from 'primeng/checkbox';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuitemComponent } from './menuitem/menuitem.component';
+import { TopbarComponent } from './topbar/topbar.component';
+import { MenuComponent } from './menu/menu.component';
+import { MainComponent } from './main/main.component';
+import { FooterComponent } from './footer/footer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DashbordComponent } from './components/dashbord/dashbord.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuitemComponent,
+    TopbarComponent,
+    MenuComponent,
+    MainComponent,
+    FooterComponent,
+    DashbordComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    BadgeModule,
+    AvatarModule,
+    HttpClientModule,
+    TableModule,
+    RippleModule,
+    PasswordModule,
+    CheckboxModule,
+    FormsModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
