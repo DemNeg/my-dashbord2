31360647c7bf1fe2265979076e803997090b5d77
DemNeg <basingoudembele@gmail.com>
2022-03-25 11:15:53 +0100

    Define menuMode,setup primeNgConfig in the rootcmp
    -Now the menu can have either static or overlay mode
    - Supported components (such as button ...) have animations ripple now
    
    - We want the user to choose which mode he/she prefer
    -  Ripple animation is disabled by default and needs to be enabled globally at the
    main component e.g. app.component.ts by injecting PrimeNGConfig

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'my-dashbord2';
+export class AppComponent implements OnInit{
+  
+  menuMode = 'static';  //to define the menu mode (static or overlay)
+
+  constructor(private primengConfig: PrimeNGConfig){}
+
+  ngOnInit(): void {
+    this.primengConfig.ripple = true;
+    document.documentElement.style.fontSize = '14px';
+  }
+
+
 }
