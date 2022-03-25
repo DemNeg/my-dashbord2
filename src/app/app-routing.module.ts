b370c59c442acd7916bae78957f53a32f49cd03d
DemNeg <basingoudembele@gmail.com>
2022-03-25 18:39:29 +0100

    UPDATE APP-ROUTING
    - Add vehicle registration form route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashbordComponent } from './components/dashbord/dashbord.component';
 import { LoginComponent } from './components/login/login.component';
+import { AddVehicleComponent } from './components/vehicle/add-vehicle/add-vehicle.component';
 import { MainComponent } from './main/main.component';
 
 const routes: Routes = [
@@ -9,6 +10,7 @@ const routes: Routes = [
     path:'',component:MainComponent,
     children:[
       {path:'',component:DashbordComponent},
+      {path:'addVehicle',component:AddVehicleComponent}
       
       
     ]
