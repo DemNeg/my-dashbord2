2594d6eca37abac984a67dacfb23b055958aabb5
DemNeg <basingoudembele@gmail.com>
2022-03-25 10:39:50 +0100

    Add routes for this components:Main,Dashbord,login
    - We basically configure the route for mainComponent and its
    childComponent => Dashbord, and also for login component.
    - Now we are able to access the home page as well as our login page.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashbordComponent } from './components/dashbord/dashbord.component';
+import { LoginComponent } from './components/login/login.component';
+import { MainComponent } from './main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',component:MainComponent,
+    children:[
+      {path:'',component:DashbordComponent},
+      
+      
+    ]
+  },
+  {path:'login',component:LoginComponent}
+  
+];
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
