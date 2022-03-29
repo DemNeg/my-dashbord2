70cf27eb5f91a234281b15d69975dce11e3e80db
DemNeg <basingoudembele@gmail.com>
2022-03-29 13:11:18 +0200

    UPDATE ADDVEHICLE_COMPONENT
    - addVehicle is now establish with correct data and form
    
    - In preview commit we had just a template with random data that are now
      replaced

--- src/app/components/vehicle/add-vehicle/add-vehicle.component.ts
+++ src/app/components/vehicle/add-vehicle/add-vehicle.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MessageService } from 'primeng/api';
+import { Vehicle } from 'src/app/api/vehicle';
 
 @Component({
   selector: 'app-add-vehicle',
@@ -9,7 +10,12 @@ import { MessageService } from 'primeng/api';
 })
 export class AddVehicleComponent implements OnInit {
   selectedState:any;
+  selectedType:any;
+  selectedFuel:any;
+  selectedMeasurement:any;
+  selectedTrack:any;
   uploadedFiles: any[] = [];
+  valSwitch: boolean = false;
   //maxSize = 1000000;
 
   constructor(private messageService: MessageService) { }
@@ -17,11 +23,32 @@ export class AddVehicleComponent implements OnInit {
   ngOnInit(): void {
   }
 
-    dropdownItems = [
-      { name: 'Option 1', code: 'Option 1' },
-      { name: 'Option 2', code: 'Option 2' },
-      { name: 'Option 3', code: 'Option 3' }
-  ];
+    dropdownTypes = [
+      { name: 'Type 1', code: 'Option 1' },
+      { name: 'Type 2', code: 'Type 2' },
+      { name: 'Type 3', code: 'Type 3' }
+    ];
+    dropdownStates = [
+      { name: 'Active', code: 'Active' },
+      { name: 'Inactive', code: 'Inactive' },
+      { name: 'Sold', code: 'Sold' },
+    ];
+    FuelTypes = [
+      { name: 'Diesel', code: 'Diesel' },
+      { name: 'Gasoline', code: 'Gasoline' },
+      { name: 'Propane', code: 'Propane' },
+      { name: 'Compressed Natural gas', code: 'Compressed Natural gas' },
+    ];
+    FuelMeasurements = [
+      { name: 'Gallons(UK)', code: 'Gallons_UK' },
+      { name: 'Gallons(USA)', code: 'Gallons_USA' },
+      { name: 'Liters', code: 'Liters' },
+    ];
+    TracksUsage_As = [
+      { name: 'Kilometers', code: 'Kilometers' },
+      { name: 'Miles', code: 'Miles' },
+      { name: 'Hours', code: 'Hours' },
+    ];
 
   onUpload(event: any) {
     for (const file of event.files) {
